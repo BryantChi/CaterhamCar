3cccb99efa3b81ccedfb191eabde85d9cd169a17
BryantChi <bryantchi.work@gmail.com>
2023-01-18 00:49:17 +0800

    [MOD] 修改調整navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,21 +2,25 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('index') }}">caterham</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05"
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05"
                 aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
+
             <div class="collapse navbar-collapse" id="navbarsExample05">
                 <ul class="navbar-nav pl-md-5 ml-auto text-center">
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="{{ route('index') }}">首頁<br>Home</a>
+                        <a class="nav-link {{ request()->is('/') ? 'active' : '' }}"
+                            href="{{ route('index') }}">首頁<br>Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->is('about') ? 'active' : '' }}" href="/about">關於我們<br>About Us</a>
+                        <a class="nav-link {{ request()->is('about') ? 'active' : '' }}" href="/about">關於我們<br>About
+                            Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->is('news*') ? 'active' : '' }}" href="{{ route('news') }}">最新消息<br>News</a>
+                        <a class="nav-link {{ request()->is('news*') ? 'active' : '' }}"
+                            href="{{ route('news') }}">最新消息<br>News</a>
                     </li>
                     {{-- <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="services.html" id="dropdown04"
@@ -28,10 +32,12 @@
                         </div>
                     </li> --}}
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->is('models') ? 'active' : '' }}" href="{{ route('models') }}">車款介紹<br>Models</a>
+                        <a class="nav-link {{ request()->is('models') ? 'active' : '' }}"
+                            href="{{ route('models') }}">車款介紹<br>Models</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->is('contact') ? 'active' : '' }}" href="{{ route('contact') }}">聯絡我們<br>Contact</a>
+                        <a class="nav-link {{ request()->is('contact') ? 'active' : '' }}"
+                            href="{{ route('contact') }}">聯絡我們<br>Contact</a>
                     </li>
                 </ul>
 

a5403d35a62081d20c33a840b7d73e99e6d55643
BryantChi <bryantchi.work@gmail.com>
2023-02-17 15:51:34 +0800

    [MOD] 優化修改v5

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -31,7 +31,7 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- Theme Style -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}?v=20230217.1">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}?v=20230217.2">
 
 </head>
 
